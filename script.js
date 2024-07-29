e033e6cee32fe938224b388ae0d1c062d23c1c7e
Harish S <115945412+Harishshanmugamm@users.noreply.github.com>
2024-07-29 16:27:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let textarea = document.getElementById("textarea");
+let currentCount = document.getElementById("current");
+let remainCount = document.getElementById("remain");
+let counter = document.getElementById("counter");
+let counter2=document.getElementById("counter-remaining");
+
+textarea.addEventListener("input", () => {
+  let currentLength = textarea.value.length;
+  currentCount.textContent = currentLength;
+
+  let remainlenth = textarea.value.length;
+  remainCount.textContent = 50 - remainlenth;
+
+  if (currentLength == 50) {
+    counter.style.color = "red";
+    counter2.style.color = "red";
+    remainCount.style.color = "red";
+    currentCount.style.color = "red";
+  } else {
+    counter.style.color = "white";
+  }
+});
